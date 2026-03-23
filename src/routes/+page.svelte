0de3755431f4b10845497d391236cb073a29e91f
deven <ddangi@ufl.edu>
2026-03-22 20:25:39 -0400

    Created initial frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="body">
+    <div class="impl">
+        <h2>Trie</h2>
+        <textarea></textarea>
+    </div>
+    <div class="impl">
+        <h2>K-d Tree</h2>
+        <textarea></textarea>
+    </div>
+</section>
+
+<style>
+    #body { 
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    h2 {
+        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-weight: bold;
+    }
+
+    textarea {
+        resize: none;
+        width: clamp(45vw, 500px, 80vw);
+        height: clamp(40vh, 500px, 60vh);
+        border: solid rgba(0, 0, 0, 0.8) 1px;
+        border-radius: 7px;
+        box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.4);
+        transition: 0.3s;
+        padding: 10px;
+        font-size: 1rem;
+    }
+
+    textarea:hover {
+        box-shadow: 3px 3px 0px rgba(0, 0, 0, 0.5);
+    }
+</style>
