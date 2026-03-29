5707b99ac83517c6dc111230447de1f6bcbbdbc2
Arya M <armlk4711@gmail.com>
2026-03-28 20:00:44 -0400

    Feat: display loading text while trie and kd-tree initialize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,64 +2,62 @@
   import { onMount, tick } from "svelte";
 
   // Setup editor TipTap
-  import {
-    createEditor,
-    EditorContent,
-  } from "svelte-tiptap";
+  import { createEditor, EditorContent } from "svelte-tiptap";
   import StarterKit from "@tiptap/starter-kit";
   import { Extension } from "@tiptap/core";
-  import { Plugin, TextSelection } from "@tiptap/pm/state"
-  import { Decoration, DecorationSet } from '@tiptap/pm/view';
+  import { Plugin, TextSelection } from "@tiptap/pm/state";
+  import { Decoration, DecorationSet } from "@tiptap/pm/view";
   import { KDTree } from "$lib/kd-tree/kd.ts";
   import { Trie } from "$lib/trie/trie.ts";
   import { numDifferentChars, splitIntoWords } from "$lib/util.ts";
+  import loading_icon from "$lib/assets/loading_icon.gif";
 
   let props = $props();
   let Kd = null;
   let trie = new Trie(props.data.content);
-  let Kd_built = false;
+  let Kd_built = $state(false);
 
   // Custom trie editor extension for detecting incorrect words
   const HighlightTyposExtensionTrie = Extension.create({
     name: "highlightTyposTrie",
 
     addProseMirrorPlugins() {
-        const typoPlugin = new Plugin({
-          state: {
-            init(_, { doc }) {
-                return createTypos(doc);
-            },
-            apply(tr, set) {
-                if (tr.docChanged) {
-                    return createTypos(tr.doc);
-                }
-
-                return set.map(tr.mapping, tr.doc);
-            },
+      const typoPlugin = new Plugin({
+        state: {
+          init(_, { doc }) {
+            return createTypos(doc);
           },
-          props: {
-            decorations(state) {
-              return typoPlugin.getState(state);
-            },
-          },
-        })
+          apply(tr, set) {
+            if (tr.docChanged) {
+              return createTypos(tr.doc);
+            }
 
-        function createTypos(doc) {
-            let typos = [];
-              let wordIndices = splitIntoWords(doc.textContent);
-              for (let {startIndex, endIndex, word} of wordIndices) {
-                let wordSearch = trie.search(word.toLowerCase());
-                if (wordSearch == undefined || wordSearch.isWord === false) {
-                  typos.push(
-                    Decoration.inline(startIndex + 1, endIndex + 1, {
-                      class: "typo",
-                    }),
-                  );
-                }
-              }
-              return DecorationSet.create(doc, typos);
+            return set.map(tr.mapping, tr.doc);
+          },
+        },
+        props: {
+          decorations(state) {
+            return typoPlugin.getState(state);
+          },
+        },
+      });
+
+      function createTypos(doc) {
+        let typos = [];
+        let wordIndices = splitIntoWords(doc.textContent);
+        for (let { startIndex, endIndex, word } of wordIndices) {
+          let wordSearch = trie.search(word.toLowerCase());
+          if (wordSearch == undefined || wordSearch.isWord === false) {
+            typos.push(
+              Decoration.inline(startIndex + 1, endIndex + 1, {
+                class: "typo",
+              }),
+            );
+          }
         }
-        return [typoPlugin];
+        return DecorationSet.create(doc, typos);
+      }
+      return [typoPlugin];
     },
   });
 
@@ -68,44 +66,44 @@
     name: "highlightTyposKd",
 
     addProseMirrorPlugins() {
-        const typoPlugin = new Plugin({
-          state: {
-            init(_, { doc }) {
-                return createTypos(doc);
-            },
-            apply(tr, set) {
-                if (tr.docChanged) {
-                    return createTypos(tr.doc);
-                }
-
-                return set.map(tr.mapping, tr.doc);
-            },
+      const typoPlugin = new Plugin({
+        state: {
+          init(_, { doc }) {
+            return createTypos(doc);
           },
-          props: {
-            decorations(state) {
-              return typoPlugin.getState(state);
-            },
-          },
-        })
-
-        function createTypos(doc) {
-            let typos = [];
-            let wordIndices = splitIntoWords(doc.textContent);
-            for (let {startIndex, endIndex, word} of wordIndices) {
-              if (Kd_built && Kd.search(word.toLowerCase()) === false) {
-                  typos.push(
-                    Decoration.inline(startIndex + 1, endIndex + 1, {
-                      class: "typo",
-                    }),
-                  );
-              }
+          apply(tr, set) {
+            if (tr.docChanged) {
+              return createTypos(tr.doc);
             }
-            return DecorationSet.create(doc, typos);
+
+            return set.map(tr.mapping, tr.doc);
+          },
+        },
+        props: {
+          decorations(state) {
+            return typoPlugin.getState(state);
+          },
+        },
+      });
+
+      function createTypos(doc) {
+        let typos = [];
+        let wordIndices = splitIntoWords(doc.textContent);
+        for (let { startIndex, endIndex, word } of wordIndices) {
+          if (Kd_built && Kd.search(word.toLowerCase()) === false) {
+            typos.push(
+              Decoration.inline(startIndex + 1, endIndex + 1, {
+                class: "typo",
+              }),
+            );
+          }
         }
-        return [typoPlugin];
+        return DecorationSet.create(doc, typos);
+      }
+      return [typoPlugin];
     },
   });
-  
+
   // Custom extension for menu with suggestion opening
   const TypoMenuPlugin = Extension.create({
     name: "typoMenu",
@@ -114,120 +112,121 @@
       return [
         new Plugin({
           state: {
-            init() { 
+            init() {
               return {
                 active: false,
                 pos: null,
                 word: "",
-                suggestions: []
-              } ;
+                suggestions: [],
+              };
             },
             apply(tr, value) {
               const openMeta = tr.getMeta("openTypoMenu");
               const closeMeta = tr.getMeta("closeTypoMenu");
               if (openMeta) {
-                return {active: true, ...openMeta};
+                return { active: true, ...openMeta };
               }
-              if (closeMeta || tr.docChanged) return { active: false, pos: null, word: "", suggestions: [] };
+              if (closeMeta || tr.docChanged)
+                return { active: false, pos: null, word: "", suggestions: [] };
               return value;
-            }
+            },
           },
           props: {
             decorations(state) {
               const { active, pos, word, suggestions } = this.getState(state);
-              
+
               if (!active) return DecorationSet.empty;
 
               return DecorationSet.create(state.doc, [
-                Decoration.widget(pos, (view) => {
-                  const anchor = document.createElement("span");
-                  anchor.style.position = "relative";
-                  anchor.style.width = "0";
-                  anchor.style.display = "inline-block";
-                  anchor.setAttribute("contenteditable", "false");
+                Decoration.widget(
+                  pos,
+                  (view) => {
+                    const anchor = document.createElement("span");
+                    anchor.style.position = "relative";
+                    anchor.style.width = "0";
+                    anchor.style.display = "inline-block";
+                    anchor.setAttribute("contenteditable", "false");
 
-                  const menu = document.createElement("div");
-                  menu.classList.add("autocorrect-menu");
-                  menu.setAttribute("contenteditable", "false");
+                    const menu = document.createElement("div");
+                    menu.classList.add("autocorrect-menu");
+                    menu.setAttribute("contenteditable", "false");
 
-                  const { left } = view.coordsAtPos(pos);
-                  const editorWidth = view.dom.clientWidth;
+                    const { left } = view.coordsAtPos(pos);
+                    const editorWidth = view.dom.clientWidth;
 
-                  if (left > editorWidth * 0.7) {
-                    menu.style.right = "0";
-                    menu.style.left = "auto";
-                  } else {
-                    menu.style.left = "0";
-                    menu.style.right = "auto";
-                  }
+                    if (left > editorWidth * 0.7) {
+                      menu.style.right = "0";
+                      menu.style.left = "auto";
+                    } else {
+                      menu.style.left = "0";
+                      menu.style.right = "auto";
+                    }
 
-                  const addToDictBtn = document.createElement("button");
-                  addToDictBtn.className = "addToDictButton";
-                  addToDictBtn.innerText = "Add to Dictionary";
-                  addToDictBtn.onmousedown = (e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    
-                    trie.insert(word);
-                    if (Kd_built) {
-                      Kd.insert(word);
-                    };
-
-                    view.dispatch(
-                      view.state.tr
-                        .insertText(word, pos - word.length, pos)
-                        .setMeta("closeTypoMenu", true)
-                    );
-                    view.focus();
-                  };
-
-                  suggestions.forEach((suggestion) => {
-                    const button = document.createElement("button");
-                    button.className = "suggestion-button";
-                    button.innerText = suggestion;
-
-                    button.onmousedown = (e) => {
+                    const addToDictBtn = document.createElement("button");
+                    addToDictBtn.className = "addToDictButton";
+                    addToDictBtn.innerText = "Add to Dictionary";
+                    addToDictBtn.onmousedown = (e) => {
                       e.preventDefault();
                       e.stopPropagation();
-                      
-                      const { tr } = view.state;
-                      const from = pos - word.length;
-                      const to = pos;
+
+                      trie.insert(word);
+                      if (Kd_built) {
+                        Kd.insert(word);
+                      }
 
                       view.dispatch(
-                      view.state.tr
-                        .insertText(suggestion, from, to)
-                        .setMeta("closeTypoMenu", true)
+                        view.state.tr
+                          .insertText(word, pos - word.length, pos)
+                          .setMeta("closeTypoMenu", true),
                       );
                       view.focus();
                     };
-                    menu.appendChild(button);
-                  })
-                  menu.appendChild(addToDictBtn);
-                  anchor.appendChild(menu)
-                  return anchor;
-                }, {
-                  side: 1,
-                  stopEvent: () => true
-                })
+
+                    suggestions.forEach((suggestion) => {
+                      const button = document.createElement("button");
+                      button.className = "suggestion-button";
+                      button.innerText = suggestion;
+
+                      button.onmousedown = (e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+
+                        const { tr } = view.state;
+                        const from = pos - word.length;
+                        const to = pos;
+
+                        view.dispatch(
+                          view.state.tr
+                            .insertText(suggestion, from, to)
+                            .setMeta("closeTypoMenu", true),
+                        );
+                        view.focus();
+                      };
+                      menu.appendChild(button);
+                    });
+                    menu.appendChild(addToDictBtn);
+                    anchor.appendChild(menu);
+                    return anchor;
+                  },
+                  {
+                    side: 1,
+                    stopEvent: () => true,
+                  },
+                ),
               ]);
-            }
-          }
-        })
-      ]
-    }
-  })
-  
+            },
+          },
+        }),
+      ];
+    },
+  });
+
   let trieEditor = $state();
   let kdEditor = $state();
 
   onMount(async () => {
     trieEditor = createEditor({
-      extensions: [
-        StarterKit,
-        HighlightTyposExtensionTrie,
-        TypoMenuPlugin
-      ],
+      extensions: [StarterKit, HighlightTyposExtensionTrie, TypoMenuPlugin],
       content: "Hello, world!",
       editorProps: {
         attributes: {
@@ -240,11 +239,16 @@
 
           if (target.classList.contains("typo")) {
             const word = target.innerText;
-            
-            const rawSuggestions = [...new Set(trie.autocorrect(word.toLowerCase()))];
+
+            const rawSuggestions = [
+              ...new Set(trie.autocorrect(word.toLowerCase())),
+            ];
 
             let suggestions = rawSuggestions
-              .sort((a, b) => numDifferentChars(word, a) - numDifferentChars(word, b))
+              .sort(
+                (a, b) =>
+                  numDifferentChars(word, a) - numDifferentChars(word, b),
+              )
               .slice(0, 5);
 
             const startPos = view.posAtDOM(target, 0);
@@ -254,8 +258,8 @@
               view.state.tr.setMeta("openTypoMenu", {
                 pos: endPos,
                 word,
-                suggestions
-              })
+                suggestions,
+              }),
             );
 
             return true;
@@ -267,11 +271,7 @@
     });
 
     kdEditor = createEditor({
-      extensions: [
-        StarterKit,
-        HighlightTyposExtensionKd,
-        TypoMenuPlugin
-      ],
+      extensions: [StarterKit, HighlightTyposExtensionKd, TypoMenuPlugin],
       content: "Hello, world!",
       editorProps: {
         attributes: {
@@ -284,8 +284,10 @@
 
           if (Kd_built && target.classList.contains("typo")) {
             const word = target.innerText;
-            
-            const rawSuggestions = [...new Set(Kd.autocorrect(word.toLowerCase()))];
+
+            const rawSuggestions = [
+              ...new Set(Kd.autocorrect(word.toLowerCase())),
+            ];
 
             let suggestions = rawSuggestions
               .sort((a, b) => a[1] - b[1])
@@ -299,8 +301,8 @@
               view.state.tr.setMeta("openTypoMenu", {
                 pos: endPos,
                 word,
-                suggestions
-              })
+                suggestions,
+              }),
             );
 
             return true;
@@ -318,20 +320,24 @@
   });
 </script>
 
-<section id="body">
-  <div class="impl">
-    <h2>Trie</h2>
-    {#if $trieEditor}
-      <EditorContent class="editor" editor={$trieEditor} />
-    {/if}
-  </div>
-  <div class="impl">
-    <h2>K-d Tree</h2>
-    {#if $kdEditor}
-      <EditorContent class="editor" editor={$kdEditor} />
-    {/if}
-  </div>
-</section>
+{#if Kd_built}
+  <section id="body">
+    <div class="impl">
+      <h2>Trie</h2>
+      {#if $trieEditor}
+        <EditorContent class="editor" editor={$trieEditor} />
+      {/if}
+    </div>
+    <div class="impl">
+      <h2>K-d Tree</h2>
+      {#if $kdEditor}
+        <EditorContent class="editor" editor={$kdEditor} />
+      {/if}
+    </div>
+  </section>
+{:else}
+  <div class="loadingText"><p>Loading...</p></div>
+{/if}
 
 <style>
   #body {
@@ -413,5 +419,16 @@
 
   :global(.addToDictButton) {
     border-bottom: none;
+  }
+
+  .loadingText {
+    width: 100vw;
+    height: 100vh;
+    font-size: 4rem;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
 </style>
